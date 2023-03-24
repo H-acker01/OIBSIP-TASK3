434260d1251f0e3e264067f2ea8a1477462baa4b
Nityanand Singh <97588565+H-acker01@users.noreply.github.com>
2023-03-24 20:53:45 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,18 @@
+
+let celcius = document.getElementById('celcius');
+let fahrenheit = document.getElementById('fahrenheit');
+
+celcius.oninput=()=>{
+  let output=(parseFloat(celcius.value)*9)/5+32;
+  fahrenheit.value =parseFloat(output.toFixed(3));
+};
+
+fahrenheit.oninput=()=>{
+  let output=(parseFloat(fahrenheit.value)-32*5)/9;
+  celcius.value =parseFloat(output.toFixed(3));
+};
+
+kelvin.oninput=()=>{
+  let output=(parseFloat(celcius.value)+273.15);
+  kelvin.value=parseFloat(output.toFixed(3));
+};
